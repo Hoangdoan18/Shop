3136d8662424d3bfdb2e9947e59dac2dbfcec7e7
Hoangdoan18 <87047994+Hoangdoan18@users.noreply.github.com>
2021-07-07 10:22:59 +0700

    First view
    First view of Shop

---
+++ Project_PRJ301_Team6/src/java/Context/DBConnection.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hungl
+ */
+public class DBConnection {
+
+    public static Connection open() {
+        try {
+            //1.Dang ki drive
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            //2.Tao va mo ket noi
+            return DriverManager.getConnection("jdbc:sqlserver://DESKTOP-TM4S549:1433;"
+                    + "databaseName=BikeStore;integratedSecurity=true");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public static void close(Connection conn, Statement stmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
